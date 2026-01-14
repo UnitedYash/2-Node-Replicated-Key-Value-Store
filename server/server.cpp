0c932c406cee7adfe57ae51dd643d47ffcabfe0f
Ayyash <ayyashahmedanhardeen@gmail.com>
2026-01-13 21:12:26 -0500

    added some handle client funcs

--- server/server.cpp
+++ server/server.cpp
@@ -4,11 +4,78 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sstream>
 #include <unistd.h>
 #include <thread>
+#include <unordered_map>
+#include <mutex>
+
+std::unordered_map<std::string, std::string> store;
+std::mutex store_mutex;
 constexpr int PORT = 8080;
 
 
+void handle_put(int client_fd, const std::string& key, const std::string& value) {
+    {
+        std::lock_guard<std::mutex> lock(store_mutex);
+        store[key] = value;
+    }
+
+    std::string ok = "OK\n";
+    send(client_fd, ok.c_str(), ok.size(), 0);
+}
+
+void handle_get(int client_fd, const std::string& key) {
+    std::string reply;
+    
+    // Use a block {} to limit the scope of the lock
+    {
+        std::lock_guard<std::mutex> lock(store_mutex);
+        auto it = store.find(key);
+        if (it != store.end()) {
+            reply = it->second + "\n";
+        } else {
+            reply = "NOT_FOUND\n";
+        }
+    } // Lock is released here automatically
+
+    // Now send the data WITHOUT holding the lock
+    send(client_fd, reply.c_str(), reply.size(), 0);
+}
+
+void process_command(int client_fd, const std::string& line) {
+    std::istringstream iss(line);
+    std::string cmd;
+    iss >> cmd;
+
+    if (cmd == "PUT") {
+        std::string key, value;
+        iss >> key >> value;
+
+        if (key.empty() || value.empty()) {
+            std::string err = "ERROR invalid PUT\n";
+            send(client_fd, err.c_str(), err.size(), 0);
+            return;
+        }
+
+        handle_put(client_fd, key, value);
+    }
+    else if (cmd == "GET") {
+        std::string key;
+        iss >> key;
+
+        if (key.empty()) {
+            std::string err = "ERROR invalid PUT\n";
+            send(client_fd, err.c_str(), err.size(), 0);
+            return;
+        }
+        handle_get(client_fd, key);
+    }
+    else {
+        std::string err = "ERROR unknown command\n";
+        send(client_fd, err.c_str(), err.size(), 0);
+    }
+}
 bool handle_client(int new_socket, std::string& buffer) {
     char temp[1024];
     ssize_t valread = recv(new_socket, temp, sizeof(temp), 0);
@@ -21,24 +88,15 @@ bool handle_client(int new_socket, std::string& buffer) {
     }
 
     buffer.append(temp, valread);
-
+    std::cout << "Buffer currently contains: [" << buffer << "]" << std::endl;
     size_t pos;
     while ((pos = buffer.find('\n')) != std::string::npos) {
         std::string line = buffer.substr(0, pos);
         buffer.erase(0, pos + 1);
 
-        std::cout << "Command: " << line << std::endl;
+        process_command(new_socket, line);
 
-        // For now: echo back
-        std::string reply = line + "\n";
-        send(new_socket, reply.c_str(), reply.size(), 0);
     }
-
-
-
-    std::cout << "Received: " << std::string_view(buffer.data(), valread) << std::endl;
-
-    send(new_socket, buffer.data(), valread, 0);
     return true; 
 }
 
